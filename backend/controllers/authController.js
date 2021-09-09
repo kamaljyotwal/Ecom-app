47bd451aa8f4d63dc1be8a733412fbe4a021fcfd
kamal Jyotwal <kkjyotwal100@gmail.com>
2021-09-09 16:22:38 +0530

    auth completion
    -all auth complete
    -admin control to access and CRUD on users DB complete in authController.js
    -auth.js | relevant imports done
    ========================
    (starting orders API in upcoming push)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,14 +4,11 @@ const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const bcrypt = require("bcryptjs");
 const tokenFunc = require("../utils/jwtToken");
 const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 // register user => api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-  const {
-    name,
-    email,
-    password
-  } = req.body;
+  const { name, email, password } = req.body;
 
   const user1 = await userSchema.create({
     name,
@@ -28,10 +25,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
 // login user => api/v1/login
 exports.loginUser = catchAsyncErrors(async (req, res, next) => {
-  const {
-    email,
-    password
-  } = req.body;
+  const { email, password } = req.body;
   const user = await userSchema
     .findOne({
       email,
@@ -96,4 +90,135 @@ exports.forgotPass = catchAsyncErrors(async (req, res, next) => {
     await user1[0].save();
     next(new ErrorHandler("Some error occured", 500));
   }
+});
+
+// reset password =>api/v1/password/reset/:id
+exports.resetPass = catchAsyncErrors(async (req, res, next) => {
+  const hashed = crypto.createHash("sha256").update(req.params.hashedTkn).digest("hex");
+
+  const user = await userSchema.findOne({
+    resetPasswordToken: hashed,
+    resetPasswordExpire: {
+      $gt: Date.now(),
+    },
+  });
+
+  if (!user) {
+    return next(new ErrorHandler("Either token is invalid or has expired", 403));
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("new password mismatch", 400));
+  }
+
+  // new password setup
+  user.password = req.body.password;
+  user.resetPasswordExpire = undefined;
+  user.resetPasswordToken = undefined;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    msg: "password updated",
+  });
+});
+
+// current user => api/v1/me
+exports.getCurrentUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await userSchema.findById(req.user._id);
+  if (user) {
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  }
+});
+
+// updating password => api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const { oldPassword, newPassword, confirmPassword } = req.body;
+
+  const user = await userSchema.findById(req.user._id).select("+password");
+  const ismatching = bcrypt.compareSync(oldPassword, user.password);
+
+  if (!ismatching) {
+    return next(new ErrorHandler("You entered wrong current password", 406));
+  } else if (ismatching) {
+    if (newPassword !== confirmPassword) {
+      return next(new ErrorHandler("Type the new password correctly", 400));
+    }
+    if (newPassword === oldPassword) {
+      return next(
+        new ErrorHandler("Try something diffrent, this is already your current password", 400)
+      );
+    }
+
+    // setting and saving new password
+    user.password = newPassword;
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Password updated successfully, login again",
+    });
+  }
+});
+
+// update user details => api/v1/updateProfile
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newData = await userSchema.findByIdAndUpdate(req.user._id, req.body, {
+    new: true,
+    useFindAndModify: false,
+    runValidators: true,
+  });
+  // updating avatar/picture to be done
+  res.status(201).json({ success: true, data: newData });
+});
+
+// ADMIN ROUTES-----------------------------------------------------------
+// get all users => api/v1/admin/allUsers
+exports.getAllUsers = catchAsyncErrors(async (req, res, next) => {
+  const user = await userSchema.find();
+  res.status(200).json({ success: true, data: user });
+});
+
+// get one use => api/v1/admin/user/:id
+exports.getuserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await userSchema.findById(req.params.id);
+  if (user == null) {
+    return next(new ErrorHandler("user does not exist"), 404);
+  }
+  res.status(200).json({ success: true, data: user });
+});
+
+// update user => api/v1/admin/user/:id
+exports.updateProfileByAdmin = catchAsyncErrors(async (req, res, next) => {
+  const newData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+  const user = await userSchema.findByIdAndUpdate(req.params.id, newData, {
+    new: true,
+    useFindAndModify: false,
+    runValidators: true,
+  });
+
+  if (user === null) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+  res.status(200).json({ success: true, message: "Update successfully" });
+});
+
+// delete product => api/v1/admin/products/:id
+exports.deleteUserByAdmin = catchAsyncErrors(async (req, res, next) => {
+  const found = await userSchema.findByIdAndDelete(req.params.id);
+  if (!found) {
+    return next(new ErrorHandler("user doesn't exist", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: `user with id: ${req.params.id} deleted successfully`,
+  });
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,11 +6,28 @@ const {
   loginUser,
   logoutUser,
   forgotPass,
+  resetPass,
+  getCurrentUser,
+  updatePassword,
+  updateProfile,
+  getAllUsers,
+  getuserDetails,
+  updateProfileByAdmin,
+  deleteUserByAdmin,
 } = require("../controllers/authController");
+const { isUserAuthenticated, checkRole } = require("../middlewares/isAuthehticated");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
 router.route("/password/forgot").post(forgotPass);
-// router.route("/password/reset/:userIdentifier")
+router.route("/password/reset/:hashedTkn").post(resetPass);
+router.route("/me").get(isUserAuthenticated, getCurrentUser);
+router.route("/password/update").post(isUserAuthenticated, updatePassword);
+router.route("/updateProfile").put(isUserAuthenticated, updateProfile);
+// admin operations
+router.route("/admin/allUsers").get(isUserAuthenticated, checkRole("admin"), getAllUsers);
+router.route("/admin/user/:id").get(isUserAuthenticated, checkRole("admin"), getuserDetails);
+router.route("/admin/user/:id").post(isUserAuthenticated, checkRole("admin"), updateProfileByAdmin);
+router.route("/admin/user/:id").delete(isUserAuthenticated, checkRole("admin"), deleteUserByAdmin);
 module.exports = router;
