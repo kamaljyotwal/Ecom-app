09afaa8d9dbc197a1c91e8d7ede12eecff291066
kamal Jyotwal <kkjyotwal100@gmail.com>
2021-11-13 05:39:38 +0530

    -bug fixed, app is now working after hosting
    -bug in back/app.js,  all routes needs to be defined above  res.sendFile logic.

--- backend/app.js
+++ backend/app.js
@@ -5,20 +5,11 @@ const errorMiddleware = require("./middlewares/error");
 var cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-// require("dotenv").config({ path: "backend/config/conf.env" })
+// require("dotenv").config({ path: "backend/config/conf.env" });
 
 // setup for env
 if (process.env.NODE_ENV != "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/conf.env" });
-}
-// console.log(process.env.NODE_ENV);
-
-if (process.env.NODE_ENV == "PRODUCTION") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
 }
 
 // middlewares
@@ -32,13 +23,20 @@ const products = require("./routes/product");
 const authroute = require("./routes/auth");
 const orderRoute = require("./routes/orderRoute");
 const paymentRoute = require("./routes/payment");
-const { dirname } = require("path");
 
 // routing
 app.use("/api/v1", products);
 app.use("/api/v1", authroute);
 app.use("/api/v1/order", orderRoute);
 app.use("/api/v1", paymentRoute);
+
+if (process.env.NODE_ENV == "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
 
 // error middleware is being used in last after routing, else not working.
 app.use(errorMiddleware);
